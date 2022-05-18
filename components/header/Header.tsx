73a4bffaf63d2a2a49129a9a5ef912429d02f756
phuvietdang <phu.dang@eastplayers.io>
2022-05-19 02:01:34 +0700

    header

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -1,6 +1,8 @@
 import Button from "components/Button/Button";
 import Icon from "components/Icon/Icon";
+import Input from "components/Input/Input";
 import Select from "components/Select/Select";
+import Image from "next/image";
 import styles from "./Header.module.scss";
 
 const locations = [
@@ -20,8 +22,8 @@ const languages = [
 const Header = () => {
   return (
     <div className={styles.header}>
-      <div className={styles.container}>
-        <div className={styles.navbar_top}>
+      <div className={styles.navbar_top}>
+        <div className={styles.content}>
           <div className={styles.left_col}>
             <Icon icon="location" size={50} />
             <Select
@@ -29,11 +31,45 @@ const Header = () => {
               placeholder="Location"
               options={locations}
             />
-            <Select options={languages} />
+            <Select options={languages} variant="no-outlined" />
           </div>
           <div className={styles.right_col}>
+            <Icon icon="business" size={20} />
+            <div>Business</div>
             <Button text="Sign up" variant="outlined" />
             <Button text="Login" />
+          </div>
+        </div>
+      </div>
+      <div className={styles.navbar_bottom}>
+        <div className={styles.content}>
+          <Image src={require("public/logo.svg")} alt="logo" />
+          <Input
+            className={styles.search}
+            variant="filled"
+            placeholder="Search"
+          />
+          <div className={styles.categories}>
+            <div className={styles.category}>
+              <Icon icon="buy-color" size={20} className={styles.icon} />
+              Buy
+            </div>
+            <div className={styles.category}>
+              <Icon icon="eat-color" size={20} className={styles.icon} />
+              Eat
+            </div>
+            <div className={styles.category}>
+              <Icon icon="camera-color" size={20} className={styles.icon} />
+              See & Do
+            </div>
+            <div className={styles.category}>
+              <Icon icon="car-color" size={20} className={styles.icon} />
+              Transport
+            </div>
+            <div className={styles.category}>
+              <Icon icon="bed-color" size={20} className={styles.icon} />
+              Stay
+            </div>
           </div>
         </div>
       </div>
