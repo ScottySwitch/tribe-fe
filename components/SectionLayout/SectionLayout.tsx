f2b6638de68704645b10bf7ea8ba37da266c2a27
phuvietdang <phu.dang@eastplayers.io>
2022-05-19 20:49:21 +0700

    update section layout

---
+++ components/SectionLayout/SectionLayout.tsx
@@ -0,0 +1,24 @@
+import classNames from "classnames";
+import React, { ReactElement } from "react";
+import styles from "./Sectionlayout.module.scss";
+
+export interface SectionLayoutProps {
+  title?: string;
+  className?: string;
+  children: ReactElement | ReactElement[];
+  backgroundColor?: string;
+}
+const SectionLayout = (props: SectionLayoutProps) => {
+  const { title, className, backgroundColor, children } = props;
+
+  const sectionlayoutClassName = classNames(styles.section_layout, className);
+  return (
+    <div className={sectionlayoutClassName}>
+      <div className={styles.container}>
+        <div className={styles.title}>{title}</div>
+        <div className={styles.content}>{children}</div>
+      </div>
+    </div>
+  );
+};
+export default SectionLayout;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,48 @@
 import InforCard from "components/InforCard/InforCard";
+import SectionLayout from "components/SectionLayout/SectionLayout";
+import { inforCardList } from "contants";
 import type { NextPage } from "next";
 import styles from "../styles/Home.module.scss";
 
 const Home: NextPage = () => {
   return (
-    <div className="flex gap-[10px] p-10">
-      <InforCard
-        imgUrl="https://picsum.photos/200/300"
-        title="Evertop Hainanese Boneless Chicken"
-        rate={4.5}
-        rateNumber={25}
-        followerNumber={500}
-        price={"$37.35"}
-        categories={["Fast food", "Desserts"]}
-        tags={["Hot deals", "Best sellers"]}
-        isVerified
-      />
+    <div>
+      <SectionLayout title="What to see">
+        <div className={styles.card_container}>
+          {inforCardList.map((card) => (
+            <InforCard
+              key={card.title}
+              imgUrl={card.imgUrl}
+              title={card.title}
+              rate={card.rate}
+              rateNumber={card.rateNumber}
+              followerNumber={card.followerNumber}
+              price={card.price}
+              categories={card.categories}
+              tags={card.tags}
+              isVerified={card.isVerified}
+            />
+          ))}
+        </div>
+      </SectionLayout>
+      <SectionLayout title="What to see">
+        <div className={styles.card_container}>
+          {inforCardList.map((card) => (
+            <InforCard
+              key={card.title}
+              imgUrl={card.imgUrl}
+              title={card.title}
+              rate={card.rate}
+              rateNumber={card.rateNumber}
+              followerNumber={card.followerNumber}
+              price={card.price}
+              categories={card.categories}
+              tags={card.tags}
+              isVerified={card.isVerified}
+            />
+          ))}
+        </div>
+      </SectionLayout>
     </div>
   );
 };
