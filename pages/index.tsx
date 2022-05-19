3a22821d7f1c90b8dd4cce92bd0390b6af2e78f7
phuvietdang <phu.dang@eastplayers.io>
2022-05-20 02:30:20 +0700

    update carousel homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import InforCard from "components/InforCard/InforCard";
 import SectionLayout from "components/SectionLayout/SectionLayout";
 import { inforCardList } from "contants";
 import type { NextPage } from "next";
+
 import styles from "../styles/Home.module.scss";
 
 const Home: NextPage = () => {
@@ -11,7 +12,7 @@ const Home: NextPage = () => {
       <SectionLayout title="Exclusive deals">
         <Carousel>
           {inforCardList?.map((card) => (
-            <div key={card.title} className="py-5">
+            <div key={card.title} className="pb-5">
               <InforCard
                 imgUrl={card.imgUrl}
                 title={card.title}
@@ -30,7 +31,7 @@ const Home: NextPage = () => {
       <SectionLayout title="Where to BUY">
         <Carousel>
           {inforCardList?.map((card) => (
-            <div key={card.title} className="py-5">
+            <div key={card.title} className="pb-5">
               <InforCard
                 imgUrl={card.imgUrl}
                 title={card.title}
@@ -49,7 +50,7 @@ const Home: NextPage = () => {
       <SectionLayout title="What to SEE">
         <Carousel>
           {inforCardList?.map((card) => (
-            <div key={card.title} className="py-5">
+            <div key={card.title} className="pb-5">
               <InforCard
                 imgUrl={card.imgUrl}
                 title={card.title}
@@ -68,7 +69,7 @@ const Home: NextPage = () => {
       <SectionLayout title="What to EAT">
         <Carousel>
           {inforCardList?.map((card) => (
-            <div key={card.title} className="py-5">
+            <div key={card.title} className="pb-5">
               <InforCard
                 imgUrl={card.imgUrl}
                 title={card.title}
