b6df3d700f10dff887329318601e64186bcf2cc0
phuvietdang <phu.dang@eastplayers.io>
2022-05-27 21:50:36 +0700

    sign up interest

--- pages/signup/setup-profile/index.tsx
+++ pages/signup/setup-profile/index.tsx
@@ -7,31 +7,125 @@ import Radio from "components/Radio/Radio";
 import Select from "components/Select/Select";
 
 import styles from "styles/Auth.module.scss";
+import { useState } from "react";
+import Image from "next/image";
+
+const Information = () => {
+  return (
+    <>
+      <Input placeholder="Your name" label="Name" />
+      <Select placeholder="Your country" />
+      <div>
+        Gender
+        <div className="flex gap-[30px] mt-2">
+          <Radio label="Male" name="gender" />
+          <Radio label="Female" name="gender" />
+          <Radio label="Others" name="gender" />
+        </div>
+      </div>
+      <Input placeholder="Birthday" />
+      <Select placeholder="Education level" />
+      <Select placeholder="Industry" />
+    </>
+  );
+};
+
+const Interesting = () => {
+  return (
+    <div>
+      <p>
+        We would love to recommend categories and content especially for you!
+        You can choose more than 1.
+      </p>
+      <br />
+      <p>Selected: 0/50</p>
+    </div>
+  );
+};
+
+export enum ProfileStep {
+  STEP_ONE = "step_one",
+  STEP_TWO = "step_two",
+}
+
+const interestingList = [
+  { avatar: "https://picsum.photos/200/300", label: "Animal" },
+  { avatar: "https://picsum.photos/200/300", label: "Travel" },
+  { avatar: "https://picsum.photos/200/300", label: "DIY" },
+  { avatar: "https://picsum.photos/200/300", label: "Festival" },
+  { avatar: "https://picsum.photos/200/300", label: "Animal" },
+  { avatar: "https://picsum.photos/200/300", label: "Travel" },
+  { avatar: "https://picsum.photos/200/300", label: "DIY" },
+  { avatar: "https://picsum.photos/200/300", label: "Festival" },
+  { avatar: "https://picsum.photos/200/300", label: "Animal" },
+  { avatar: "https://picsum.photos/200/300", label: "Travel" },
+  { avatar: "https://picsum.photos/200/300", label: "DIY" },
+  { avatar: "https://picsum.photos/200/300", label: "Festival" },
+  { avatar: "https://picsum.photos/200/300", label: "Animal" },
+  { avatar: "https://picsum.photos/200/300", label: "Travel" },
+  { avatar: "https://picsum.photos/200/300", label: "DIY" },
+  { avatar: "https://picsum.photos/200/300", label: "Festival" },
+  { avatar: "https://picsum.photos/200/300", label: "Animal" },
+  { avatar: "https://picsum.photos/200/300", label: "Travel" },
+  { avatar: "https://picsum.photos/200/300", label: "DIY" },
+  { avatar: "https://picsum.photos/200/300", label: "Festival" },
+  { avatar: "https://picsum.photos/200/300", label: "Animal" },
+  { avatar: "https://picsum.photos/200/300", label: "Travel" },
+  { avatar: "https://picsum.photos/200/300", label: "DIY" },
+  { avatar: "https://picsum.photos/200/300", label: "Festival" },
+  { avatar: "https://picsum.photos/200/300", label: "Animal" },
+  { avatar: "https://picsum.photos/200/300", label: "Travel" },
+  { avatar: "https://picsum.photos/200/300", label: "DIY" },
+  { avatar: "https://picsum.photos/200/300", label: "Festival" },
+  { avatar: "https://picsum.photos/200/300", label: "Animal" },
+  { avatar: "https://picsum.photos/200/300", label: "Travel" },
+  { avatar: "https://picsum.photos/200/300", label: "DIY" },
+  { avatar: "https://picsum.photos/200/300", label: "Festival" },
+];
 
 const SetupProfilePage = () => {
+  const [step, setStep] = useState<ProfileStep>(ProfileStep.STEP_ONE);
   const router = useRouter();
 
   return (
     <div className={styles.auth}>
       <Modal visible={true} backdrop={false} width={580}>
         <div className={styles.form_container}>
-          <ModalHeader alignTitle="center">
-            Almost there... set up your profile
-          </ModalHeader>
-          <div className={styles.body}>
-            <Input placeholder="Your name" label="Name" />
-            <Select placeholder="Your country" />
-            <div>
-              Gender
-              <div className="flex gap-[30px] mt-2">
-                <Radio label="Male" name="gender" />
-                <Radio label="Female" name="gender" />
-                <Radio label="Others" name="gender" />
+          <ModalHeader
+            alignTitle={step === ProfileStep.STEP_ONE ? "center" : "left"}
+          >
+            {step === ProfileStep.STEP_ONE ? (
+              <div>Almost there... set up your profile</div>
+            ) : (
+              <div>
+                <div>Hello Anna Nhun,</div>
+                <div>What are you most interested in lately?</div>
               </div>
-            </div>
-            <Input placeholder="Birthday" />
-            <Select placeholder="Education level" />
-            <Select placeholder="Industry" />
+            )}
+          </ModalHeader>
+
+          <div className={styles.body}>
+            {step === ProfileStep.STEP_ONE && <Information />}
+            {step === ProfileStep.STEP_TWO && (
+              <>
+                <Interesting />
+                <div className={styles.interesting}>
+                  {interestingList.map((item: any) => (
+                    <div key={item.value} className={styles.interesting_item}>
+                      <Image
+                        src={item.avatar}
+                        alt=""
+                        layout="fixed"
+                        width={50}
+                        height={50}
+                        className={styles.avatar}
+                      />
+                      <div>{item.label}</div>
+                    </div>
+                  ))}
+                </div>
+              </>
+            )}
 
             <div className={styles.actions}>
               <div
@@ -43,7 +137,11 @@ const SetupProfilePage = () => {
               <Button
                 className="w-1/2"
                 text="Next"
-                onClick={() => router.push("/signup/setup-profile")}
+                onClick={() => {
+                  step === ProfileStep.STEP_ONE
+                    ? setStep(ProfileStep.STEP_TWO)
+                    : router.push("/");
+                }}
               />
             </div>
           </div>
