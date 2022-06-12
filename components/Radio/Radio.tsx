6fbb492cb5b5cb423fde108bf24a9e4ce7a7abab
phuvietdang <phu.dang@eastplayers.io>
2022-06-12 12:00:16 +0700

    fix build

--- components/Radio/Radio.tsx
+++ components/Radio/Radio.tsx
@@ -1,16 +1,16 @@
-import { Item } from "framer-motion/types/components/Reorder/Item";
-import { UseFormRegisterReturn } from "react-hook-form";
-import styles from "./Radio.module.scss";
+import { Item } from "framer-motion/types/components/Reorder/Item"
+import { UseFormRegisterReturn } from "react-hook-form"
+import styles from "./Radio.module.scss"
 
 export interface RadioProps extends React.HTMLProps<HTMLInputElement> {
-  label?: string;
-  id?: string;
-  className?: string;
-  register: UseFormRegisterReturn;
+  label?: string
+  id?: string
+  className?: string
+  register?: UseFormRegisterReturn
 }
 
 const Radio = (props: RadioProps) => {
-  const { label, id, className, register, ...rest } = props;
+  const { label, id, className, register, ...rest } = props
 
   return (
     <label className={`${styles.container} ${className}`} htmlFor={id || label}>
@@ -18,7 +18,7 @@ const Radio = (props: RadioProps) => {
       <input type="radio" id={id || label} {...register} {...rest} />
       <span className={styles.checkmark} />
     </label>
-  );
-};
+  )
+}
 
-export default Radio;
+export default Radio
