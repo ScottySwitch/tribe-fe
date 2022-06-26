f22ff61f46fa37c0247a81859d966be0462c2d71
phuvietdang <phu.dang@eastplayers.io>
2022-06-26 23:28:44 +0700

    update img domain

--- next.config.js
+++ next.config.js
@@ -5,7 +5,8 @@ module.exports = {
   reactStrictMode: true,
   images: {
     domains: ['picsum.photos', 'bamboohealth.s3.ap-southeast-1.amazonaws.com',
-    'platform-lookaside.fbsbx.com', 'lh3.googleusercontent.com'],
+      'platform-lookaside.fbsbx.com', 'lh3.googleusercontent.com', "https://meeko-cms.thewandergroup.com.sg/"
+    ],
   },
   i18n: {
     locales: ['en', 'sg'],
