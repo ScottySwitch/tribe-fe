f8a985fc014fbf0882b70e9b62b0e0690d6ecae9
scottish-ep <scottish.foldep@gmail.com>
2022-07-14 18:49:14 +0700

    push code

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ module.exports = {
   reactStrictMode: true,
   images: {
     domains: ['meeko-cms.thewandergroup.com.sg', 'picsum.photos', 'bamboohealth.s3.ap-southeast-1.amazonaws.com',
-      'platform-lookaside.fbsbx.com', 'lh3.googleusercontent.com', 'tribes-be.s3.ap-southeast-1.amazonaws.com', 'res.klook.com'
+      'platform-lookaside.fbsbx.com', 'lh3.googleusercontent.com', 'tribes-be.s3.ap-southeast-1.amazonaws.com', 'res.klook.com','pix8.agoda.net', 'dimg04.c-ctrip.com',
     ],
   },
   i18n: {
