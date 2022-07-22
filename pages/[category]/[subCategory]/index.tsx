85a577e00cda2dfc1c8d0fc85f0ce55bb3ad2c1f
scottish-ep <scottish.foldep@gmail.com>
2022-07-22 15:34:26 +0700

    fix bug

--- pages/[category]/[subCategory]/index.tsx
+++ pages/[category]/[subCategory]/index.tsx
@@ -15,15 +15,21 @@ import CategoryLinkApi from "services/category-link";
 import BannerApi from "services/banner";
 import Loader from "components/Loader/Loader";
 import useTrans from "hooks/useTrans";
-import { formatListingArray } from "utils";
+import { formatBanner, formatCategoryLink, formatListingArray } from "utils";
 import { UserInforContext } from "Context/UserInforContext";
 import Button from "components/Button/Button";
 import Filter, { IFilter } from "components/Filter/Filter";
 
 import styles from "styles/Home.module.scss";
+import Select from "components/Select/Select";
+import TabsHorizontal, { ITab } from "components/TabsHorizontal/TabsHorizontal";
+interface IType {
+  [key: string]: any;
+}
+[];
 
 const SubCategoryPage = (context) => {
-  const { listingBanners, category, categoryLink } = context;
+  const { category, categoryLink } = context;
 
   const trans = useTrans();
   const router = useRouter();
@@ -41,6 +47,10 @@ const SubCategoryPage = (context) => {
     maxRating: undefined,
   };
 
+  const [bannerArray, setBannergArray] = useState<IType[]>([]);
+  const [categoryLinkArray, setCategoryLinkArray] = useState<ITab[]>([])
+  const [currentSubCategory, setCurrentSubCategory] = useState(categoryLink);
+
   const [loading, setLoading] = useState(true);
   const [pagination, setPagination] = useState(defaultPagination);
   const [listings, setListings] = useState<{ [key: string]: any }[]>([]);
@@ -52,10 +62,40 @@ const SubCategoryPage = (context) => {
   )?.finalTabLabel;
 
   useEffect(() => {
+    const getData = async () => {
+      const dataBanners = await BannerApi.getBannerCustom({
+        categories: category,
+        limit: 12,
+        page: 1,
+      });
+      const rawListBanners = formatBanner(get(dataBanners, "data.data"));
+      setBannergArray(rawListBanners);
+
+      const dataCategoryLinks =
+      await CategoryLinkApi.getCategoryLinksByCategorySlug(category);
+      let categoryLinkArray: any = [
+        {
+          label: "All",
+          value: "all",
+          slug: "all",
+          icon: "https://picsum.photos/200/300",
+        },
+      ];
+      // const rawListBanners = get(dataBanners, "data.data");
+      const rawListCategory = formatCategoryLink(get(dataCategoryLinks, "data.data"));
+      categoryLinkArray =
+      isArray(rawListCategory) &&
+      categoryLinkArray.concat(rawListCategory);
+      setCategoryLinkArray(categoryLinkArray)
+    };
+    getData()
+  }, []);
+
+  useEffect(() => {
     const getBizListings = async () => {
       const params = {
         category,
-        categoryLinks: [categoryLink],
+        categoryLinks: categoryLink,
         page: pagination.page,
         location,
         ...filter,
@@ -79,7 +119,7 @@ const SubCategoryPage = (context) => {
     location && getBizListings();
 
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [filter, location, pagination.page]);
+  }, [filter, location, pagination.page, currentSubCategory]);
 
   if (loading) {
     return (
@@ -94,6 +134,19 @@ const SubCategoryPage = (context) => {
     e && setFilter({ ...filter, ...e });
   };
 
+  const handleChangeSubCategory = (e) => {
+    setCurrentSubCategory(e);
+    router.replace(
+      {
+        pathname: `/${category}/${e}`,
+      },
+      undefined,
+      { shallow: true }
+    );
+    // getDataBizlisting(category, e, page)
+  };
+
+
   return (
     <div>
       <SectionLayout className="pt-0">
@@ -104,7 +157,7 @@ const SubCategoryPage = (context) => {
           {categoryLink}
         </div>
         <Carousel responsive={homeBannerResponsive}>
-          {listingBanners?.map((img, index) => (
+          {bannerArray?.map((img, index) => (
             <div
               key={index}
               className={styles.banner_card}
@@ -124,6 +177,37 @@ const SubCategoryPage = (context) => {
       </SectionLayout>
       <SectionLayout className={styles.tab_filter}>
         <div className={styles.tab_filter_container}>
+        <div className="flex">
+            <TabsHorizontal
+              tablist={
+                Array.isArray(categoryLinkArray)
+                  ? categoryLinkArray.slice(0, 5)
+                  : []
+              }
+              type="secondary-no-outline"
+              selectedTab={currentSubCategory}
+              className="pt-[6px]"
+              onCurrentTab={handleChangeSubCategory}
+            />
+            {/* <Select
+              placeholder="More"
+              isSearchable={false}
+              width={50}
+              className={styles.sub_category_more}
+              variant="no-outlined"
+              size="small"
+              options={
+                Array.isArray(listCategoryLink) ? listCategoryLink.slice(5) : []
+              }
+              controlStyle={{ fontWeight: "bold", fontSize: "16px" }}
+              placeholderStyle={{
+                fontWeight: "bold",
+                fontSize: "16px",
+                color: "#a4a8b7",
+              }}
+              onChange={(e) => handleChangeSubCategory(e.value)}
+            /> */}
+          </div>
           <Button
             width={180}
             size="small"
@@ -181,44 +265,43 @@ const SubCategoryPage = (context) => {
 export async function getServerSideProps(context) {
   const category = context.query.category;
   const categoryLink = context.query.subCategory;
-  const dataBanners = await BannerApi.getBannerByCategory(category);
-  const dataCategoryLinks =
-    await CategoryLinkApi.getCategoryLinksByCategorySlug(category);
-  let categoryLinkArray: any = [
-    {
-      label: "All",
-      value: "all",
-      slug: "all",
-      icon: "https://picsum.photos/200/300",
-    },
-  ];
-  const rawListBanners = get(dataBanners, "data.data");
-  const rawListCategory = get(dataCategoryLinks, "data.data");
-  const listBannerArray =
-    Array.isArray(rawListBanners) &&
-    rawListBanners.map((item) => ({
-      imgUrl: item.image_url,
-      linkActive: item.link_active,
-    }));
-  console.log(listBannerArray);
-  let arrayRawListCategoryLink =
-    Array.isArray(rawListCategory) &&
-    rawListCategory.map((item) => ({
-      icon: get(item, "attributes.logo.url") || null,
-      label: get(item, "attributes.label"),
-      value: get(item, "attributes.value"),
-      slug: get(item, "attributes.value"),
-    }));
-  categoryLinkArray =
-    Array.isArray(arrayRawListCategoryLink) &&
-    categoryLinkArray.concat(arrayRawListCategoryLink);
+  // const dataBanners = await BannerApi.getBannerByCategory(category);
+  // const dataCategoryLinks =
+  //   await CategoryLinkApi.getCategoryLinksByCategorySlug(category);
+  // let categoryLinkArray: any = [
+  //   {
+  //     label: "All",
+  //     value: "all",
+  //     slug: "all",
+  //     icon: "https://picsum.photos/200/300",
+  //   },
+  // ];
+  // const rawListBanners = get(dataBanners, "data.data");
+  // const rawListCategory = get(dataCategoryLinks, "data.data");
+  // const listBannerArray =
+  //   Array.isArray(rawListBanners) &&
+  //   rawListBanners.map((item) => ({
+  //     imgUrl: item.image_url,
+  //     linkActive: item.link_active,
+  //   }));
+  // let arrayRawListCategoryLink =
+  //   Array.isArray(rawListCategory) &&
+  //   rawListCategory.map((item) => ({
+  //     icon: get(item, "attributes.logo.url") || null,
+  //     label: get(item, "attributes.label"),
+  //     value: get(item, "attributes.value"),
+  //     slug: get(item, "attributes.value"),
+  //   }));
+  // categoryLinkArray =
+  //   Array.isArray(arrayRawListCategoryLink) &&
+  //   categoryLinkArray.concat(arrayRawListCategoryLink);
   return {
     props: {
       // bizListings: listingArray,
       category: category,
       categoryLink: categoryLink,
-      listingBanners: listBannerArray,
-      listCategoryLink: categoryLinkArray,
+      // listingBanners: listBannerArray,
+      // listCategoryLink: categoryLinkArray,
     },
   };
 }
